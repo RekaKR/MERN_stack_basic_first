f25afe1e51e42f6580ee7be70fe718cb03d885c4
RekaKR <RekaKR@users.noreply.github.com>
2021-06-30 19:34:44 +0200

    Create post.js, update posts.js

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const Post = require('../models/post')
 
 router.get('/', (req, res) => {
   res.send('We are on posts')
+})
+
+router.post('/', (req, res) => {
+  console.log(req.body)
 })
 
 //Can have multiple here
